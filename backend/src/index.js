a9c5334c858f31a248211d45ee3b6af26817640f
fidelis27 <thiago.fidelis27@gmail.com>
2020-03-25 14:11:51 -0300

    add backend

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+const port = 3333;
+app.listen(port ,() => console.log(`Executando na porta ${port}`) );
